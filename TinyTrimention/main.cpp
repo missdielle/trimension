1f498bb17ea446ccfaa3e7592d90f11c4ca34b02
missdielle <missdielle@gmail.com>
2016-09-20 20:44:45 -0700

    SDL include is compiling
    havn't written any graphics yet

--- TinyTrimention/main.cpp
+++ TinyTrimention/main.cpp
@@ -2,11 +2,17 @@
 #include "board.h"
 #include "Vertex.h"
 #include "Game.h"
+#include <SDL.h>
+
 
 using namespace std;
 //int MAXTURNS = 20;
 
-int main(){
+//some SDL globals:
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* argv[]){
 
 	int playerCount = 0;
 	char strategy = NULL;
@@ -38,6 +44,15 @@ int main(){
 				bool defensive = false;
 			}
 		}
+	}
+
+	//SDL window (following code is from lazyfoo.net tutorial)
+	SDL_Window* window = NULL;
+	//surface on window
+	SDL_Surface * screenSurface = NULL; 
+	//initialize window inside error check
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 	}
 
 	game.playGame(playerCount, defensive);
